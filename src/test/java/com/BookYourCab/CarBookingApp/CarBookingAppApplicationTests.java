6f5dba72df655dd01e3af542a5ae79d6d01e95bd
ProfessorXcommunity <irealarijit2@gmail.com>
2025-02-12 11:46:24 +0530

    first commit

---
+++ src/test/java/com/BookYourCab/CarBookingApp/CarBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookYourCab.CarBookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
