2cf5680e2f4e966656642ea193241a32a716f269
ProfessorXcommunity <irealarijit2@gmail.com>
2025-03-02 21:31:56 +0530

    added OSRM API

--- src/main/java/com/BookYourCab/CarBookingApp/Services/Implementations/DistanceOSRMImpl.java
+++ src/main/java/com/BookYourCab/CarBookingApp/Services/Implementations/DistanceOSRMImpl.java
@@ -1,14 +1,43 @@
 package com.BookYourCab.CarBookingApp.Services.Implementations;
 
 import com.BookYourCab.CarBookingApp.Services.DistanceService;
+import lombok.Data;
 import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 @Service
 public class DistanceOSRMImpl implements DistanceService {
+
+    private static final String OSRM_API_BASE_URL =
+            "http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219";
+
     @Override
     public double calculateDistance(Point src, Point dest) {
 //        todo add api link for OSRM
-        return 0;
+        try{
+            String uri = src.getX()+","+src.getY()+";"+dest.getX()+","+dest.getY();
+            OSRMResponseDto osrmResponseDto = RestClient.builder()
+                    .baseUrl(OSRM_API_BASE_URL)
+                    .build()
+                    .get()
+                    .uri(uri)
+                    .retrieve()
+                    .body(OSRMResponseDto.class);
+            return osrmResponseDto.getRoutes().get(0).getDistance()/1000.0;
+        } catch (Exception e) {
+            throw new RuntimeException("Error getting data from OSRM "+e.getMessage());
+        }
+
     }
+}
+@Data
+class OSRMResponseDto{
+    private List<OSRMRoute> routes;
+}
+@Data
+class OSRMRoute{
+    private Double distance;
 }
