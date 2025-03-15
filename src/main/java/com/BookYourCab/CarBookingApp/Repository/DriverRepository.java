33b90ce8831a3846a69e5b50139437d02c9f058b
ProfessorXcommunity <irealarijit2@gmail.com>
2025-03-15 17:25:34 +0530

    debugged requestRide

--- src/main/java/com/BookYourCab/CarBookingApp/Repository/DriverRepository.java
+++ src/main/java/com/BookYourCab/CarBookingApp/Repository/DriverRepository.java
@@ -4,23 +4,24 @@ import com.BookYourCab.CarBookingApp.Entity.Driver;
 import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface DriverRepository extends JpaRepository<Driver,Long> {
-    @Query(value = "SELECT d.* , ST_Distance(d.current_location,:pickUpLocation) AS distance"+
-            "FROM drivers d "+
-            "WHERE d.available = true AND ST_Within(d.current_location,:pickUpLocation,10000)"+
-            "ORDER BY DISTANCE "+
+    @Query(value = "SELECT d.* , ST_Distance(d.current_location,:pickUpLocation) AS distance "+
+            "FROM driver d "+
+            "WHERE d.available = true AND ST_DWithin(d.current_location,:pickUpLocation,10000)"+
+            "ORDER BY distance "+
             "LIMIT 10",nativeQuery = true)
-    List<Driver> findNearestTenDriver(Point pickUpLocation);
+    List<Driver> findNearestTenDriver(@Param("pickUpLocation") Point pickUpLocation);
 
     @Query(value = "SELECT d.* " +
-            "FROM drivers d "+
-            "WHERE d.available = true AND ST_Within(d.current_location,:pickUpLocation,15000)" +
+            "FROM driver d "+
+            "WHERE d.available = true AND ST_DWithin(d.current_location,:pickUpLocation,15000)" +
             "ORDER BY d.rating DESC"+
             "LIMIT 10",nativeQuery = true)
-    List<Driver> findTenNearbyTopRatedDriver(Point pickUpLocation);
+    List<Driver> findTenNearbyTopRatedDriver(@Param("pickUpLocation") Point pickUpLocation);
 }
