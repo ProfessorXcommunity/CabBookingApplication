84510526ba7188a7477cdd7085bcc07bb82881fb
ProfessorXcommunity <irealarijit2@gmail.com>
2025-03-24 01:04:11 +0530

    added prod & dev profiles

--- src/test/java/com/BookYourCab/CarBookingApp/TestContainerConfiguration.java
+++ src/test/java/com/BookYourCab/CarBookingApp/TestContainerConfiguration.java
@@ -1,6 +1,7 @@
 package com.BookYourCab.CarBookingApp;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +9,14 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
+@Slf4j
 public class TestContainerConfiguration {
     @Bean
     @ServiceConnection
      PostgreSQLContainer<?> postgreSQLContainer(){
         var image = DockerImageName.parse("postgis/postgis:12-3.0")
                 .asCompatibleSubstituteFor("postgres");
+        log.info("Test container image created successfully!");
         return new PostgreSQLContainer<>(image);
     }
 
